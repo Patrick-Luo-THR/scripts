e541e6cf868b186fb78edf81d0f340adb892ab0c
Patrick-Luo-THR <90332208+Patrick-Luo-THR@users.noreply.github.com>
2021-12-17 10:12:30 -0500

    Added imageid

--- script/tag.sh
+++ script/tag.sh
@@ -2,6 +2,6 @@ source .env
 
 # aws ecr get-login-password --region "$REGION" | docker login --username AWS --password-stdin "$REPO"
 
-docker tag c5c43018cb09 ${REPO}/${TARGET}-${ARCH}-${DEPLOYMENT}:${TAG}
+docker tag ${IMAGEID} ${REPO}/${TARGET}-${ARCH}-${DEPLOYMENT}:${TAG}
 
 
